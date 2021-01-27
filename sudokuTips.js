77faec2fc29e5d3ba0c2f245c6c0a8693b90c8f9
hage <hage.ycy@gmail.com>
2021-01-27 19:25:51 +0800

    add sudokuTips.js

--- sudokuTips.js
+++ sudokuTips.js
@@ -4,10 +4,13 @@
 
 if($response.statusCode == 200 && $request.method == 'GET') {
     const bodyObj = JSON.parse($response.body);
-    bodyObj.extended_params.tips_start_easy = 100
-    bodyObj.extended_params.tips_start_medium = 100
-    bodyObj.extended_params.hints_start_expert = 100
-    bodyObj.extended_params.hints_start_hard = 100
+    bodyObj["extended_params"]["hints_start_expert"] = 10
+    bodyObj["extended_params"]["hints_start_hard"] = 10
+    bodyObj["extended_params"]["tips_rewarded_expert"] = 30
+    bodyObj["extended_params"]["tips_rewarded_hard"] = 30
+    bodyObj["postbid"]["admob_postbid"]["rewarded_adunits"] = {}
+    bodyObj["postbid"]["admob_postbid"]["inter_adunits"] = {}
+    bodyObj["postbid"]["admob_postbid"]["banner_adunits"] = {}
     $done({body: JSON.stringify(bodyObj)})
 } else {
     $done({})
